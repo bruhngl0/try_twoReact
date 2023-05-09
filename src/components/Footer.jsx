4a45050e6d28544a818f31c8f1ecc96c33328ad3
bruh.ngl0 <adityashrm500@gmail.com>
2023-05-10 02:54:53 +0530

    footer 3:30

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Heading, Stack, VStack, HStack, Button} from '@chakra-ui/react'
+import { Box, Heading, Stack, VStack, HStack, Button, Input, Text} from '@chakra-ui/react'
 import {AiOutlineSend} from "react-icons/ai"
 
 const Footer = () => {
@@ -8,12 +8,21 @@ const Footer = () => {
 
         <Stack direction={['column' , 'row']}>
 
-            <VStack>
-                <Heading size={'md'} textTransform={'uppercase'}>
+            <VStack alignItems={'strect'} w={'full'} px={'4'}>
+                <Heading size={'md'} textTransform={'uppercase'} textAlign={['center','left']}>
                     Subscribe to our newsletter
                 </Heading>
 
-                <HStack>
+                <HStack borderBottom={'2px solid white'} py= "2">
+
+                    <Input 
+                        placeholder='Enter Email Here...' 
+                        border={'none'} 
+                        borderRadius={'none'} 
+                        outline={'none'} 
+                        focusBorderColor='none'
+                        
+                    />
                     <Button
                        p={'0'}
                        colorScheme='purple'
@@ -25,6 +34,43 @@ const Footer = () => {
                     </Button>
                 </HStack>
             </VStack>
+
+            <VStack 
+                w={'full'}
+                borderLeft={['none', '1px solid white']}
+                borderRight={['none', '1px solid white']}>
+
+                   <Heading  textTransform={"uppercase"} textAlign={'cent'}>
+                    VIDEO HUB
+                   </Heading>
+                   <Text>All rights reserved</Text>
+
+            </VStack>
+
+            <VStack w={'full'}>
+
+                <Heading size={'md'} textTransform={'uppercase'}>
+                    Social Media
+                </Heading>
+
+                <Button target={'blank'} variant={'link'} colorScheme={'purple'}>
+                <a href= "https://www.youtube.com">YouTube</a>
+                </Button>
+
+                <Button target={'blank'} variant={'link'} colorScheme={'purple'}>
+                <a href= "www.instagram.com">Instagram</a>
+                </Button>
+
+                <Button target={'blank'} variant={'link'} colorScheme={'purple'}>
+                <a href= "www.github.com">GitHub</a>
+                </Button>
+
+
+            </VStack>
+
+           
+
+            
 
         </Stack>
 
