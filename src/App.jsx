7099eacca49bd6bf95fdf44d66ee559b128e6762
bruh.ngl0 <adityashrm500@gmail.com>
2023-05-10 04:56:32 +0530

    upload 3:45

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
 import Videos from "./components/Videos";
+import Upload from "./components/Upload";
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path= "/" element= {<Home />}/>
         <Route path= "/videos" element={<Videos />}/>
+        <Route path= "/upload" element={<Upload />}/>
       </Routes>
       <Footer />
     </Router>
